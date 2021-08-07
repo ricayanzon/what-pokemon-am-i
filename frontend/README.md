6b5290ad8531a93b2f4675c515ef39c3d9100312
thicc-rabbit <ricayanzon@gmail.com>
2021-08-07 17:32:47 +0200

    chore: update frontend

--- frontend/README.md
+++ frontend/README.md
@@ -1,6 +1,6 @@
-# Frontend
+# WhatPokemonAmI
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.3.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.8.
 
 ## Development server
 
--- frontend/e2e/src/app.e2e-spec.ts
+++ frontend/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('frontend app is running!');
+    expect(await page.getTitleText()).toEqual('what-pokemon-am-i app is running!');
   });
 
   afterEach(async () => {
